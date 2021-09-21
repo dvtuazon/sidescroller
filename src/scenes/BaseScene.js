4145c338f2116b5cafe170ef74702c624cd5e5e5
David <dvtuazon@gmail.com>
2021-09-21 12:19:10 -0700

    sprite resize

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -1,7 +1,9 @@
 let collisionLayer;
+let w;
+let h;
 class BaseScene extends Phaser.Scene {
     constructor() {
-        super({ key: "BaseScene" });
+        super({ key: 'BaseScene' });
     }
 
     preload() {
@@ -22,25 +24,39 @@ class BaseScene extends Phaser.Scene {
         this.backgroundLayer = this.map.createLayer('backgroundLayer', tileset);
         collisionLayer = this.map.createLayer('collisionLayer', tileset);
         this.goalLayer = this.map.createLayer('goalLayer', tileset);
-        collisionLayer.setCollisionByExclusion([-1]);
 
         player = new Player({ scene: this, x: 100, y: 100, sprite: 'warrior' });
+        w = player.width;
+        h = player.height;
+        // const cropRect = new Phaser.GameObjects.Rectangle(player.scene, 0, 0, 16, 32);
+        player.setBodySize(16, 32);
+        collisionLayer.setCollisionByExclusion([-1]);
         this.physics.add.collider(player, collisionLayer);
     }
 
-    // update() {
-    //     if (keyA.isDown) {
-    //         player.setVelocityX(-100);
-    //     } else if (keyD.isDown) {
-    //         player.setVelocityX(100);
-    //     } else {
-    //         player.setVelocityX(0);
-    //     }
+    update() {
+        if (keyA.isDown) {
+            player.setVelocityX(-100);
+        } else if (keyD.isDown) {
+            player.setVelocityX(100);
+        } else {
+            player.setVelocityX(0);
+        }
     
-    //     if (keyW.isDown) {
-    //         player.setVelocityY(-100);
-    //     } else if (keyS.isDown) {
-    //         player.setVelocityX(0);
-    //     }
-    // }
+        if (keyW.isDown) {
+            player.setVelocityY(-100);
+        } else if (keyS.isDown) {
+            player.setVelocityX(0);
+        }
+
+        // if (this.input.x < w && this.input.y < h)
+        // {
+        //     player.x = this.input.x;
+        //     player.y = this.input.y;
+        //     cropRect.x = this.input.x;
+        //     cropRect.y = this.input.y;
+
+        //     player.updateCrop();
+        // }
+    }
 }
